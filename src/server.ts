eb892520b633eb6532e016720081807c7289e146
Simple Guruji <kumaramit957014@gmail.com>
2025-07-01 21:00:35 +0800

    make database connection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import config from 'config'
 import app from './app'
 import logger from './config/logger'
+import { initDb } from './config/db'
 
-const startServer = () => {
+const startServer = async () => {
     const port: number = config.get('server.port') || 5502
 
     try {
+        await initDb()
+        logger.info('Database connection established.')
         app.listen(port, () => logger.info(`Server running  on port ${port}.`))
     } catch (error) {
         console.error('Error starting server', error)
@@ -13,4 +16,4 @@ const startServer = () => {
     }
 }
 
-startServer()
+void startServer()
