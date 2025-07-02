7c5995ff7c86348f7d819f0d6f3b3ba21fc2a1e8
Simple Guruji <kumaramit957014@gmail.com>
2025-07-02 21:10:07 +0800

    add authentication

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import { globalErrorHandler } from './common/middlewares/globalErrorHandler'
 import categoryRouter from './category/category.routes'
 
 const app = express()
 app.use(express.json())
-
+app.use(cookieParser())
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
 })
